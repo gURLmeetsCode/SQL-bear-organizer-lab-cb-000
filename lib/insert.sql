f060618ed120105ed33343b16540e354a2b07907
natasha kelly <kellynatashav@yahoo.com>
2017-06-04 21:51:10 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocoloate", 2, "F", "Brown", "sleepy", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "F", "Brown", "calm", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Black", "friendly", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "Black", "calm",  1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "M", "Black", "aggressive", 0);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "White", "sneaky", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "M", "White", "nice", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("null", 9, "F", "Brown", "calm", 0);
