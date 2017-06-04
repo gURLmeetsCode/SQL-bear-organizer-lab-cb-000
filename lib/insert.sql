bd3c42b4a846c1b96b5270507601196a6b8e0b85
natasha kelly <kellynatashav@yahoo.com>
2017-06-04 21:58:35 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,9 @@
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocoloate", 2, "F", "Brown", "sleepy", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "F", "Brown", "calm", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "Black", "friendly", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "Black", "calm",  1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa", 12, "M", "Black", "aggressive", 0);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5, "M", "White", "sneaky", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "M", "White", "nice", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("null", 9, "F", "Brown", "calm", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
